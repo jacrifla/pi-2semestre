2b52a90facc56d1f9aae26fe1e29b8d06debeeb0
jacrifla <jaquelini_cris@hotmail.com>
2022-05-10 00:39:01 -0300

    Arquivos iniciais

---
+++ php/footer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <div class="row">
+            <h3>Mapa do Site</h3>
+            <div class="col">
+                <h4>Conceitos</h4>
+                <ul>
+                    <li><a href="#">Cidadania</a></li>
+                    <li><a href="#">Democracia vs Autoritarismo</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h4>Tripartite</h4>
+                <ul>
+                    <li><a href="#">Executivo</a></li>
+                    <li><a href="legislativo.html">Legislativo</a></li>
+                    <li><a href="#">Judiciário</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h4>Cargos</h4>
+                <ul>
+                    <li><a href="#">Presidente</a></li>
+                    <li><a href="#">Senadores</a></li>
+                    <li><a href="#">Deputados Federais</a></li>
+                    <li><a href="#">Juízes do STF</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h4>Candidaturas</h4>
+                <ul>
+                    <li><a href="#">Presidente</a></li>
+                    <li><a href="#">Senadores</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <ul>
+                    <li><a href="#">Partidos</a></li>
+                    <li><a href="#">Linha do Tempo - Presidentes</a></li>
+                    <li><a href="#">Galeria - Juízes do STF</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Quem Somos</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="row text-center">
+            <small>© 2022 - Ursinhos Felizes em Chamas - Todos os direitos reservados</small>
+        </div>
+
+        <div class="row text-center">
+            <small>Clique aqui <a href="quem-somos.html"><img src="imagens/120069.png"></a> veja nossas normas de
+                uso</small>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ php/header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+    <?php
+        <header class="text-center">
+            <img class="img-fluid" src="imagens/portal-intelecto.svg" alt="">
+        </header>
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0d0d0d;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="imagens/icon-menu48x48.png" alt="" width="30" height="24">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Conceitos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Cidadania</a></li>
+                        <li><a class="dropdown-item" href="#">Democracia vs Autoritarismo</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Tripartite
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Executivo</a></li>
+                        <li><a class="dropdown-item" href="legislativo.html">Legislativo</a></li>
+                        <li><a class="dropdown-item" href="#">Judiciário</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cargos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Presidente</a></li>
+                        <li><a class="dropdown-item" href="#">Senadores</a></li>
+                        <li><a class="dropdown-item" href="#">Deputados Federais</a></li>
+                        <li><a class="dropdown-item" href="#">Juízes do STF</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Candidaturas
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Presidente</a></li>
+                        <li><a class="dropdown-item" href="#">Senadores</a></li>
+                    </ul>
+                    </li>
+
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">Partidos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">Linha do Tempo</a>
+                    </li><li class="nav-item">
+                    <a href="#" class="nav-link">Galeria</a>
+                    </li><li class="nav-item">
+                    <a href="#" class="nav-link">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">Quem Somos</a>
+                    </li>
+                </ul>
+                
+                </div>
+            </div>
+        </nav>
+    ?>
+    
+</body>
+</html>
