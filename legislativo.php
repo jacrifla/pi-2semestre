34b66a952d8881aa8109fa1fe1b28505ff187749
jacrifla <jaquelini_cris@hotmail.com>
2022-06-06 19:26:27 -0300

    mudando as pags para php

--- legislativo.php
+++ legislativo.php
@@ -13,7 +13,9 @@
 
     </head>
 
-    <body id="legislativo">
+    <body id="legislativo"> 
+        <?php include_once('php/header.php'); ?>
+
         <div class="container-fluid">
 
             <article class="d-flex row">
@@ -28,6 +30,7 @@
             </article>
 
         </div>
+        <?php include_once('php/footer.php'); ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
