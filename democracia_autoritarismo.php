3e601a3439f25667169baf5bbda7e98e823fb013
Daniel Santolin Uhr Martins <91101320+Daniel-Santolin@users.noreply.github.com>
2022-06-06 18:48:33 -0300

    Adicionando as páginas Cidadania e Democracia

---
+++ democracia_autoritarismo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+    <title>Democracia vs Autoritarismo</title>
+
+</head>
+
+<body>
+
+
+    <?php include_once('php/header.php'); ?>
+
+
+    <main>
+        <article class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <h2 class="text-center fs-1 m-3">Democracia</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
+                    </p>
+                </div>
+                <div class="col-12 col-md-1 align-self-center">
+                    <p class="text-center fw-bold fs-1">X</p>
+                </div>
+                <div class="col-12 col-md">
+                    <h2 class="text-center fs-1 m-3">Autoritarismo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
+                    </p>
+                </div>
+            </div>
+        </article>
+    </main>
+
+
+    <?php include_once('php/footer.php'); ?>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
