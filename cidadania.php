b52b6c5cf3a8e22d379d8c54e289e31ebb84dc3d
Daniel-Santolin <91101320+Daniel-Santolin@users.noreply.github.com>
2022-06-13 00:50:02 -0300

    Texto da página Cidadania

--- cidadania.php
+++ cidadania.php
@@ -27,17 +27,28 @@
     <article class="container mx-auto">
       <h1 class="row justify-content-center m-3 fs-1">Cidadania</h1>
       <div class="clearfix">
-        <div class="col-md-6 float-md-start mx-3 ms-md-3">
+        <div class="col-md-6 float-md-start mx-3 mb-1 me-md-4">
           <img class="w-100" src="imagens/vote-certo.jpg" alt="Imagem de bonecos palito com escrito: Vote Certo">
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-        </p>
+        <p>A palavra cidadania em sua origem etimológica remete à palavra “cidade”, o que em outras palavras significa dizer que está diretamente ligada aos princípios que compõem o conceito do que é ser parte de uma comunidade. Ser cidadão significa
+          usufruir de diversas vantagens que essa coletividade possa oferecer, mas também implica dizer que temos obrigações para com os demais membros do grupo. No entanto é importante destacar que existem dois conceitos distintos de cidadania:</p>
+        <ul class="m-4">
+          <li><strong>Cidadania Formal:</strong> é aquela utilizada no contexto internacional para se referir à nacionalidade de uma pessoa, por exemplo: podemos dizer que uma pessoa possui cidadania brasileira, cidadania americana, cidadania alemã, entre outras;</li>
+          <li><strong>Cidadania Substantiva:</strong> é aquela que diz respeito aos direitos e deveres do cidadão. Ela engloba os aspectos histórico-sociais de um povo, podendo ser diferente em outras culturas. Ela é geralmente explicitada por meio de uma
+            Constituição como no caso do Brasil. Aqui estaremos nos referindo a esse conceito em particular.</li>
+        </ul>
+        <p>De um modo geral o conceito sofre alterações substanciais com o decorrer das eras. Na Europa Medieval, por exemplo, ser cidadão era algo próximo a se dizer que o indivíduo era parte de do patrimônio de seu senhor. Podemos dizer que embora
+          a democracia tenha começado a tomar forma na Grécia antiga, foi somente no Pós Segunda Guerra que tivemos um crescimento considerável nos direitos que compõem o conceito. Enquanto que no Brasil a mudança demorou um pouco mais, sendo o fim
+          do regime militar (1964-1985) o período das maiores conquistas nesta área.</p>
+        <p>No entanto, ainda hoje podemos notar uma contradição na maneira em que as pessoas tratam o conceito. Dissemos anteriormente que cidadania era sobre direitos e deveres do cidadão. Hoje com advento da tecnologia e um crescimento cada vez
+          maior do individualismo, vemos as pessoas buscando seus direitos como cidadãos, mas deixando de lado os deveres.</p>
+        <p>Cada vez mais vemos uma sociedade ser pouco a pouco reconstruída através da inversão que ocorre quando o cidadão passa a não entender que seus deveres também são uma conquista a ser valorizada. Dentre esses deveres destacamos o voto como o
+          principal. Para a maioria da população o importante é o ato em si de votar, eles não compreendem que o voto começa muito antes do momento da urna. O voto começa na responsabilidade do indivíduo de estudar e se manter informado, no
+          compromisso do cidadão para com a veracidade daquilo que está sendo proposto. Mas esse compromisso, infelizmente, é negligenciado pela maioria. O que torna o resultado das eleições algo muito parecido com um leilão de troca de benefícios e
+          favores pessoais ou pior ainda, na massificação da população a seguir cegamente alguém baseados quase que inteiramente na fé e no carisma apresentado.</p>
+        <p>Exercer Cidadania é algo que dá trabalho, são necessárias muitas horas de busca, leitura e pesquisa. É necessário que haja uma real valorização das conquistas pagas com o sangue de nossos antepassados. Precisamos compreender que podemos
+          ser mais do que só um voto, podemos abrir os olhos daquelas à nossa volta e juntos sermos responsáveis e conscientes na hora que a sociedade precisar de nós, deixando de lado o egoísmo e o negativismo que juntos constroem desculpas para a
+          nossa falta de ação.</p>
       </div>
 
     </article>
