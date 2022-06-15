d050b93aec1233705804f62370427617e430cd48
Daniel-Santolin <91101320+Daniel-Santolin@users.noreply.github.com>
2022-06-15 01:20:26 -0300

    Adicionando texto Demo X Auto + referências

--- democracia_autoritarismo.php
+++ democracia_autoritarismo.php
@@ -2,60 +2,96 @@
 <html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
 
 
 
-    <title>Democracia vs Autoritarismo</title>
+  <title>Democracia vs Autoritarismo</title>
 
 </head>
 
 <body>
 
 
-    <?php include_once('php/header.php'); ?>
+  <?php include_once('php/header.php'); ?>
 
 
-    <main>
-        <article class=" my-5 container-fluid">
-            <div class="row">
-                <div class="col-12 col-md">
-                    <h2 class="text-center fs-1 m-3">Democracia</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-                    </p>
-                </div>
-                <div class="col-12 col-md-1 align-self-center">
-                    <p class="text-center fw-bold fs-1">X</p>
-                </div>
-                <div class="col-12 col-md">
-                    <h2 class="text-center fs-1 m-3">Autoritarismo</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga illo necessitatibus id consequatur ratione, voluptas atque at, tempore maiores earum animi, totam eaque, adipisci doloremque neque dicta ullam aperiam. Harum.
-                    </p>
-                </div>
-            </div>
-        </article>
-    </main>
+  <main>
+    <article class=" my-5 container">
+      <div class="row">
+        <div class="col-12 col-md border border-2 rounded p-4">
+          <h2 class="text-center fs-1 m-3">Democracia</h2>
+          <p>O termo democracia nasceu juntamente com o conceito de cidadania na Grécia e significa basicamente “poder do povo” ou “povo no poder”. Ela prega a ideia de que o Estado deve ser regido em prol da maioria da população, apesar desse
+            conceito, por sua vez, vir sofrendo alterações consideráveis ao longo dos anos, culminando com a <a href="https://pt.wikipedia.org/wiki/Revolu%C3%A7%C3%A3o_Francesa" target="_blank">Revolução Francesa</a> e posteriormente com o <a
+              href="https://pt.wikipedia.org/wiki/Feminismo" target="_blank">Movimento Feminista</a>. “A democracia moderna prevê a criação de um Estado de Direito, onde todos são,
+            a princípio, livres e iguais, não importando a origem, a classe social, a cor ou a religião” - <cite><a href="https://brasilescola.uol.com.br/sociologia/democracia.htm" target="_blank">Porfírio</a></cite>. No entanto, é importante
+            destacar que existe mais de um
+            tipo de democracia, podendo ser dividida em:</p>
+          <ul>
+            <li class="p-1"><strong>Democracia Direta:</strong> forma clássica de democracia, onde não haviam eleições e os cidadãos debatiam e alteravam as leis de maneira direta, isto é, sem um representante. Só era possível devido ao número
+              reduzido de
+              cidadãos, considerando
+              também que as mulheres não eram consideradas parte deste grupo.</li>
+            <li class="p-1"><strong>Democracia Representativa:</strong> conforme a sociedade foi crescendo foi ficando cada vez mais difícil sustentar a democracia direta, portanto foi necessário que fossem implementadas eleições de representantes
+              legais que
+              defendessem os valores e
+              ideais, bem como os projetos de seus eleitores. Esses representantes seriam então substituídos de tempos em tempos para evitar que houvessem abusos e deveria haver também uma Constituição que servisse de âncora para as futuras
+              alterações.
+              Também chamada de Democracia Indireta, hoje essa é a forma de democracia mais comum em todo o mundo, sendo sua maior fraqueza a corrupção.</li>
+            <li class="p-1"><strong>Democracia Participativa:</strong> podendo ser chamada de Democracia Semidireta, essa forma de democracia reúne elementos dos dois tipos anteriores. Aqui existem eleições de representantes, mas as decisões são
+              tomadas pelo
+              povo que participa de
+              assembleias locais ou através de plebiscitos que funcionam como uma consulta da opinião popular.</li>
+          </ul>
+        </div>
 
-    <p class="referencias-footer text-center m-0">
-      <small class="paleta-preto-color ">Acesse também: <a class="paleta-preto-color fw-bold" href="referencias.html#Conceitos" target="_blank">Referências dessa página</a></small>
-    </p>
 
-    <?php include_once('php/footer.php'); ?>
+        <div class="col-12 col-md-1 align-self-center">
+          <p class="text-center fw-bold fs-1">X</p>
+        </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
+        <div class="col-12 col-md border border-2 rounded p-4">
+          <h2 class="text-center fs-1 m-3">Autoritarismo</h2>
+          <p>Enquanto que na democracia temos um regime marcado pela discordância, participação popular e conflito; em um Regime Autoritário temos constantemente, uma democracia de fachada onde ocorrem eleições, porém sem participação popular
+            ou
+            com sua
+            participação fraldada.</p>
+          <p>A ausência total ou parcial dos seguintes elementos básicos de uma democracia, pode indicar que o regime praticado é na verdade autoritário:</p>
+          <ul>
+            <li>Liberdade de expressão e de imprensa;</li>
+            <li>Possibilidade de voto e elegibilidade política;</li>
+            <li>Liberdade de associação política;</li>
+            <li>Acesso à informação;</li>
+            <li>Eleições idôneas.</li>
+          </ul>
+          <p>O processo que leva à perda desses direitos geralmente se inicia na própria democracia à medida em esta continua a falhar na tentativa de conciliar duas partes opostas levando a uma total intolerância entre ambas e, fatalmente, à
+            instituição de um Regime Autoritário como resposta à parte derrotada, que passa a ser obrigada a se submeter.</p>
+          <p>Por fim, vale ressaltar que o Autoritarismo pode se dar de diversas maneiras, como por exemplo: Ditadura, Totalitarismo, Fascismo, Nazismo, Monarquia*, entre outros.</p>
+          <small>* Esse regime pode ser autoritário, como nos casos das monarquias medievais europeias; ou não, como é o caso atual do <a href="https://pt.wikipedia.org/wiki/Reino_Unido" target="_blank">Reino
+              Unido</a>, que é uma monarquia constitucional unitária parlamentar.</small>
+        </div>
+      </div>
+
+    </article>
+  </main>
+
+  <p class="referencias-footer text-center m-0">
+    <small class="paleta-preto-color ">Acesse também: <a class="paleta-preto-color fw-bold" href="referencias.html#Conceitos" target="_blank">Referências dessa página</a></small>
+  </p>
+
+  <?php include_once('php/footer.php'); ?>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
